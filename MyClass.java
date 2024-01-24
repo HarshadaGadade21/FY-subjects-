d68b1ad552e96528852ddbaf6e1f2bd0563cd68a
HarshadaGadade21 <157357956+HarshadaGadade21@users.noreply.github.com>
2024-01-24 20:06:28 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+package com.example.lib;
+
+public class MyClass {
+    //write here your java codes
+
+    /*
+    hello my friends
+    all
+    these lines
+    are not being executed.
+    they are ignored!
+     * */
+}
