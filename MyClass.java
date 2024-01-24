9bb5b2bd12fcebb65f1629155c1a838dcbd2f2c7
HarshadaGadade21 <157357956+HarshadaGadade21@users.noreply.github.com>
2024-01-24 21:30:37 +0530

    Add files via upload

--- MyClass.java
+++ MyClass.java
@@ -1,13 +1,8 @@
 package com.example.lib;
 
 public class MyClass {
-    //write here your java codes
-
-    /*
-    hello my friends
-    all
-    these lines
-    are not being executed.
-    they are ignored!
-     * */
+    public static void main(String[] args){
+        // Java Output
+        System.out.print("Hello My Friends");
+    }
 }
